f80273f519e3bda5432f3fa7347138212fa1d757
Eiler Ka-Nid Yodla-Ong Poulsen <microhive@outlook.com>
2018-06-05 00:41:15 +0200

    Added BlazorStrap support

---
+++ BlazorStrap/content/blazorStrapInterop.js
@@ -0,0 +1,52 @@
+﻿Blazor.registerFunction('BlazorStrap.BlazorStrapInterop.Log', function (message) {
+    console.log('message: ', message);
+    return true;
+});
+
+Blazor.registerFunction('BlazorStrap.BlazorStrapInterop.ChangeBody', function (classname) {
+    document.body.className = classname;
+    return true;
+});
+
+Blazor.registerFunction('BlazorStrap.BlazorStrapInterop.Popper', function (target, popperId, arrow, placement) {
+    var reference = document.getElementById(target);
+    var popper = document.getElementById(popperId);
+    showPopper(reference, popper, arrow, placement);
+    return true;
+});
+
+Blazor.registerFunction('BlazorStrap.BlazorStrapInterop.Tooltip', function (target, tooltip, arrow, placement) {
+    var reference = document.getElementById(target);
+    reference.addEventListener('mouseover', function () {
+        tooltip.className = "tooltip fade show bs-popover-" + placement;
+        showPopper(reference, tooltip, arrow, placement);
+    });
+    reference.addEventListener('mouseout', function () {
+        tooltip.className = "tooltip hide";
+    });
+    return true;
+});
+
+function showPopper(reference, popper, arrow, placement) {
+    var thePopper = new Popper(reference, popper,
+        {
+            placement,
+            modifiers: {
+                offset: {
+                    offset: 0
+                },
+                flip: {
+                    behavior: 'flip'
+                },
+                arrow: {
+                    element: arrow,
+                    enabled: true
+                },
+                preventOverflow: {
+                    boundary: 'scrollParent'
+                }
+
+            }
+        }
+    );
+}
---
+++ BlazorStrap/util/BlazorStrapInterop.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Blazor;
+using Microsoft.AspNetCore.Blazor.Browser.Interop;
+
+namespace BlazorStrap.util
+{
+    public class BlazorStrapInterop
+    {
+        public static bool ChangeBody(string classname)
+        {
+            return RegisteredFunction.Invoke<bool>("BlazorStrap.BlazorStrapInterop.ChangeBody", classname);
+        }
+        public static bool Log(string message)
+        {
+            return RegisteredFunction.Invoke<bool>("BlazorStrap.BlazorStrapInterop.Log", message);
+        }
+        public static bool Popper(string target, string popper, ElementRef arrow, string placement)
+        {
+            return RegisteredFunction.Invoke<bool>("BlazorStrap.BlazorStrapInterop.Popper", target, popper, arrow, placement);
+        }
+        public static bool Tooltip(string target, ElementRef tooltip, ElementRef arrow, string placement)
+        {
+            return RegisteredFunction.Invoke<bool>("BlazorStrap.BlazorStrapInterop.Tooltip", target, tooltip, arrow, placement);
+        }
+    }
+}
